a3c43717e334018c47339bce0175fc5b2fc7c1f3
DscNNcT <joaquin.saavedraa@mail.udp.cl>
2023-05-31 14:50:11 -0400

    tercer commit, creacion de modelos

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MenuTable
+admin.site.register(MenuTable)
+
+from .models import BookingTable
+admin.site.register(BookingTable)
